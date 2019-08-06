1ceabbdd42186421aed57cf83e12b5a1136f90c0
lovepk <2275701932@qq.com>
2019-08-06 17:44:48 +0800

    第二章学习

--- ch2/echo4/main.go
+++ ch2/echo4/main.go
@@ -13,14 +13,20 @@ import (
 	"strings"
 )
 
+// sep和n变量分别是指向对应命令行标志参数变量的指针
 var n = flag.Bool("n", false, "omit trailing newline")
 var sep = flag.String("s", " ", "separator")
 
+// go run main.go -n -s l a b c
 func main() {
 	flag.Parse()
-	fmt.Print(strings.Join(flag.Args(), *sep))
+	fmt.Println(flag.Args())
+	// 命令行参数-n使用的时候 *n为true，不使用的时候*n为false
+	fmt.Println(*n)
+	fmt.Println(*sep)
+	fmt.Println(strings.Join(flag.Args(), *sep))
 	if !*n {
-		fmt.Println()
+		fmt.Println("说明使用了命令行参数-n")
 	}
 }
 
--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -6,7 +6,8 @@
 //!+
 
 package tempconv
-
+// 对于每一个类型T，都有一个对应的类型转换操作T(x)，用于将x转为T类型（译注：如果T是指针类型，可能会需要用小括弧包装T，比如(*int)(0)）
+// 只有当两个类型的底层基础类型相同时，才允许这种转型操作，或者是两者都是指向相同底层结构的指针类型，这些转换只改变类型而不会影响值本身
 // CToF converts a Celsius temperature to Fahrenheit.
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
